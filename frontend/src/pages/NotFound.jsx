e4468b91030a60f788901f034f461b7e763805e2
hzjanuary <nhhoang03012004@gmail.com>
2025-11-03 15:26:50 +0700

    update 404 notfound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen text-center bg-slate-50">
+      <img
+        src="404_NotFound.png"
+        alt="not found"
+        className="max-wfull mb-6 w-96"
+      />
 
-export default NotFound
+      <p className="text-xl font-semibold">Bạn đang đi sai hướng</p>
+
+      <a
+        href="/"
+        className="inline-block px-6 py-3 mt-6 font-medium text-white trasition shadow-md bg-primary rounded-2xl hover:bg-primary-dark"
+      >
+        Quay về trang chủ
+      </a>
+    </div>
+  );
+};
+
+export default NotFound;
