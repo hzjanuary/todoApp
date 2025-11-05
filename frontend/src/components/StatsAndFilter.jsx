8f47c17b99bf09e1750c3b13c20f88aefb5ed63e
hzjanuary <nhhoang03012004@gmail.com>
2025-11-05 16:23:24 +0700

    update

--- frontend/src/components/StatsAndFilter.jsx
+++ frontend/src/components/StatsAndFilter.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Badge } from './ui/badge';
 
-const StatsAndFilter = (completedTaskCount = 0, activeTaskCount = 0, filter = "all") => {
+const StatsAndFilter = (
+  completedTaskCount = 0,
+  activeTaskCount = 0,
+  filter = 'all',
+) => {
   return (
     <div className="flex flex-col items-start justify-between gap-4 sm:flex-row sm:items-center">
-      
+      {/* phần thống kê */}
+      <div className="flex-gap 3">
+        <Badge variant="secondary" className=""></Badge>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StatsAndFilter
+export default StatsAndFilter;
